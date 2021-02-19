5d26f6e0896af8c91cad125a9f04dd71db63b15f
Velyana Dimova <70020646+villdimova@users.noreply.github.com>
2021-02-19 08:16:31 +0200

    Add files via upload

---
+++ ADO.NET/08.IncreaseMinionAge/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Linq;
+
+namespace _08.IncreaseMinionAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "Server =.; Database = MinionsDB; Integrated Security = true";
+
+            using SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            int[] minionsIndexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            string updateMinions = @" UPDATE Minions
+                                                   SET Name = UPPER(LEFT(Name, 1)) + SUBSTRING(Name, 2, LEN(Name)), Age += 1
+                                                    WHERE Id = @Id";
+            
+            foreach (var index in minionsIndexes)
+            {
+                using var command = new SqlCommand(updateMinions, connection);
+                command.Parameters.AddWithValue("@Id", index);
+                command.ExecuteNonQuery();
+            }
+
+            string selectMinionsQuery = @"SELECT Name, Age FROM Minions";
+
+            var selectMinionsCommand = new SqlCommand(selectMinionsQuery, connection);
+
+            using var reader = selectMinionsCommand.ExecuteReader();
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{ reader["Name"]} {reader["Age"]}");
+                }
+            }
+           
+        }
+    }
+}
---
+++ ADO.NET/09.IncreaseAgeSP/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace IncreaseAgeSP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "Server =.; Database = MinionsDB; Integrated Security = true";
+
+            using SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            int id = int.Parse(Console.ReadLine());
+
+            string query = @"EXEC usp_GetOlder @Id";
+
+            using var command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@Id", id);
+            command.ExecuteNonQuery();
+
+            string selectQuery = @"SELECT Name, Age FROM Minions WHERE Id = @Id";
+            using var selectCommand = new SqlCommand(selectQuery, connection);
+            selectCommand.Parameters.AddWithValue("@Id", id);
+            using var reader =selectCommand.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine($"{reader["Name"]} – {reader["Age"]} years old");
+            }
+           
+;        }
+    }
+}
