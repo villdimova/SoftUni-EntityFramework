b9860244f90d20097f21c66e6c8bb05fbee47ecc
Velyana Dimova <70020646+villdimova@users.noreply.github.com>
2021-02-22 21:06:55 +0200

    Add files via upload

---
+++ IntroductionToEntityFramework/03.EmployeesFullInformation/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using SoftUni.Data;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            var context = new SoftUniContext();
+
+            Console.WriteLine(GetEmployeesFullInformation(context));
+        }
+
+       
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context
+                .Employees
+                .OrderBy(e => e.EmployeeId)
+                .Select(e => new
+                {
+                    Name = String.Join(" ", e.FirstName, e.LastName, e.MiddleName), 
+                    JobTitle=e.JobTitle,
+                    Salary=Math.Round(e.Salary,2)
+
+                })
+                .ToList();
+
+            foreach (var e in employees)
+            {
+                sb.AppendLine($"{e.Name} {e.JobTitle} {e.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ IntroductionToEntityFramework/04.EmployeesWithSalaryOver50000/StartUp.cs
@@ -0,0 +1,76 @@
+﻿using SoftUni.Data;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            var context = new SoftUniContext();
+
+            Console.WriteLine(GetEmployeesWithSalaryOver50000(context));
+        }
+
+        /*
+
+         Your task is to extract all employees with salary over 50000. 
+        Return their first names and salaries in format “{firstName} - {salary}”.
+        Salary must be rounded to 2 symbols, after the decimal separator. 
+        Sort them alphabetically by first name.
+
+         */
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context
+                .Employees
+                .OrderBy(e => e.EmployeeId)
+                .Select(e => new
+                {
+                    Name = String.Join(" ", e.FirstName, e.LastName, e.MiddleName), 
+                    JobTitle=e.JobTitle,
+                    Salary=Math.Round(e.Salary,2)
+
+                })
+                .ToList();
+
+            foreach (var e in employees)
+            {
+                sb.AppendLine($"{e.Name} {e.JobTitle} {e.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context
+                .Employees
+                .Where(e => e.Salary > 50000)
+                .Select(e => new
+                {
+                    Name = e.FirstName,
+                    Salary = e.Salary
+                })
+                .OrderBy(e => e.Name)
+                .ToList();
+
+            foreach (var empl in employees)
+            {
+                sb.AppendLine($"{empl.Name} - {empl.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+
+        }
+    }
+}
