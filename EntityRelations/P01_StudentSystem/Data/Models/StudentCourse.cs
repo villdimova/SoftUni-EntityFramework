0acad6984dae83d8ca68d46b678b13f3a63235a1
Velyana Dimova <70020646+villdimova@users.noreply.github.com>
2021-03-01 16:46:51 +0200

    Update StudentCourse.cs

--- EntityRelations/P01_StudentSystem/Data/Models/StudentCourse.cs
+++ EntityRelations/P01_StudentSystem/Data/Models/StudentCourse.cs
@@ -14,7 +14,7 @@ namespace P01_StudentSystem.Data.Models
         public Student Student { get; set; }
         
         [ForeignKey (nameof(Course))]
-        public int CorseId { get; set; }
+        public int CourseId { get; set; }
         public Course Course { get; set; }
     }
 }
