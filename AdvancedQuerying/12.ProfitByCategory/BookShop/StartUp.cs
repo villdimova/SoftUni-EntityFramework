8587b7ae3508d7dbc74b801b2786cba9876175ab
Velyana Dimova <70020646+villdimova@users.noreply.github.com>
2021-03-08 10:01:43 +0200

    Add files via upload

---
+++ AdvancedQuerying/12.ProfitByCategory/BookShop/StartUp.cs
@@ -0,0 +1,63 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            // DbInitializer.ResetDatabase(db);
+
+            Console.WriteLine(GetTotalProfitByCategory(db));
+            
+
+        }
+
+        public static string GetTotalProfitByCategory(BookShopContext context)
+        {
+
+            var books = context.Categories
+                .Select(x => new
+                {
+                    category = x.Name,
+                    totalPrice = x.CategoryBooks.Select(y => y.Book.Copies * y.Book.Price).Sum()
+
+
+                })
+                .OrderByDescending(x=>x.totalPrice).ThenBy(x=>x.category)
+                .ToList();
+
+               
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var b in books)
+            {
+                sb.AppendLine($"{b.category} ${b.totalPrice:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+           
+           
+
+        }
+
+        //Return the total profit of all books by category. 
+        //Profit for a book can be calculated by multiplying its number of copies by the price per single book. 
+        //Order the results by descending by total profit for category and ascending by category name.
+
+
+
+
+
+
+    }
+}
---
+++ AdvancedQuerying/13.MostRecentBooks/BookShop/StartUp.cs
@@ -0,0 +1,70 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            // DbInitializer.ResetDatabase(db);
+
+            Console.WriteLine(GetMostRecentBooks(db));
+
+
+        }
+
+        public static string GetMostRecentBooks(BookShopContext context)
+        {
+            var categories = context.Categories
+                .Select(x => new
+                {
+                    category = x.Name,
+                    books = x.CategoryBooks.Select(b => new
+                    {
+                        name = b.Book.Title,
+                        releaseDate = b.Book.ReleaseDate.Value
+
+                    })
+                    .OrderByDescending(x => x.releaseDate)
+                    .Take(3)
+                    .ToList()
+
+                })
+                .OrderBy(x => x.category).ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var c in categories)
+            {
+                sb.AppendLine($"--{c.category}");
+
+                foreach (var b in c.books)
+                {
+                    sb.AppendLine($"{b.name} ({b.releaseDate.Year})");
+                }
+            }
+
+            return sb.ToString().TrimEnd();
+
+
+
+        }
+
+        //Get the most recent books by categories.The categories should be ordered by name alphabetically.
+        // Only take the top 3 most recent books from each category - ordered by release date (descending). 
+        // Select and print the category name, and for each book – its title and release year.
+
+
+
+
+
+    }
+}
