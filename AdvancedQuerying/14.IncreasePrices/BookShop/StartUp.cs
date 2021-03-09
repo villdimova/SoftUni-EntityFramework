fab83ba7c33c36dd0408daaeb36cad1f6e68854c
Velyana Dimova <70020646+villdimova@users.noreply.github.com>
2021-03-09 10:59:41 +0200

    Add files via upload

---
+++ AdvancedQuerying/14.IncreasePrices/BookShop/StartUp.cs
@@ -0,0 +1,48 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            // DbInitializer.ResetDatabase(db);
+
+         IncreasePrices(db);
+
+
+        }
+
+        public static void IncreasePrices(BookShopContext context)
+        {
+
+            var books = context.Books.ToList();
+
+            foreach (var b in books)
+            {
+                if (b.ReleaseDate.Value.Year<2010)
+                {
+                    b.Price += 5;
+                }
+            }
+
+            context.SaveChanges();
+            
+
+        }
+
+        //Increase the prices of all books released before 2010 by 5.
+
+
+
+
+    }
+}
---
+++ AdvancedQuerying/15.RemoveBooks/BookShop/StartUp.cs
@@ -0,0 +1,52 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+             DbInitializer.ResetDatabase(db);
+
+           var result= RemoveBooks(db);
+
+            Console.WriteLine(result);
+
+        }
+
+        public static int RemoveBooks(BookShopContext context)
+        {
+
+            var removedBooks = context.Books.Where(x => x.Copies < 4200);
+
+            var count = removedBooks.Count();
+
+
+            context.Books.RemoveRange(removedBooks);
+
+            context.SaveChanges();
+
+
+
+            return count;
+
+
+
+        }
+
+        //Remove all books, which have less than 4200 copies. 
+        //Return an int - the number of books that were deleted from the database.
+
+
+
+
+    }
+}
