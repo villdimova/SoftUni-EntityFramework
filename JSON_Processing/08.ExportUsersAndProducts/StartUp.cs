0600d9401b2e70e08e4b3440a9f9d95668e7edde
Velyana Dimova <70020646+villdimova@users.noreply.github.com>
2021-03-13 15:40:32 +0200

    Add files via upload

---
+++ JSON_Processing/08.ExportUsersAndProducts/StartUp.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var db = new ProductShopContext();
+
+            //db.Database.EnsureDeleted();
+            //db.Database.EnsureCreated();
+
+            var inputJson = File.ReadAllText("./../../../Datasets/categories-products.json");
+
+            Console.WriteLine(GetUsersWithProducts(db));
+        }
+
+        public static string GetUsersWithProducts(ProductShopContext context)
+        {
+
+            var allUsers = context.Users
+                  .ToList()
+                  .Where(x => x.ProductsSold.Any(y => y.Buyer != null))
+                  .Select(u => new
+                  {
+                      firstName = u.FirstName,
+                      lastName = u.LastName,
+                      age = u.Age,
+                      soldProducts = new
+                      {
+                          count = u.ProductsSold.Where(z => z.Buyer != null).Count(),
+                          products = u.ProductsSold.Where(z => z.Buyer != null).Select(p => new
+                          {
+                              name = p.Name,
+                              price = p.Price
+
+                          })
+
+                      }
+                  }).OrderByDescending(x => x.soldProducts.count)
+                  .ToList();
+
+            var resultObject = new
+            {
+                usersCount = allUsers.Count(),
+                users = allUsers,
+            };
+
+            var jsonSettings = new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented,
+                NullValueHandling = NullValueHandling.Ignore
+
+            };
+
+
+            var resultJson = JsonConvert.SerializeObject(resultObject, jsonSettings);
+
+            return resultJson;
+        }
+    }
+}
+
+//Get all users who have at least 1 sold product with a buyer. 
+//Order them in descending order by the number of sold products with a buyer. 
+//Select only their first and last name, age and for each product - name and price. 
+//Ignore all null values.
+//Export the results to JSON. 
+//Follow the format below to better understand how to structure your data. 
---
+++ JSON_Processing/09.ImportSuppliers/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AutoMapper;
+using CarDealer.Data;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var db = new CarDealerContext();
+
+
+            //db.Database.EnsureDeleted();
+            // db.Database.EnsureCreated();
+
+            var inputJson = File.ReadAllText("./../../../Datasets/suppliers.json");
+
+            Console.WriteLine(ImportSuppliers(db,inputJson));
+
+        }
+
+
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+
+        {
+            var suppliers = JsonConvert.DeserializeObject<List<Supplier>>(inputJson);
+
+            context.Suppliers.AddRange(suppliers);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Count}.";
+
+
+        }
+    }
+}
