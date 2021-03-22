ed1af8bb4f41a0e766c6ef112a7d2fee0eb463a3
Velyana Dimova <70020646+villdimova@users.noreply.github.com>
2021-03-22 17:04:11 +0200

    Add files via upload

---
+++ ExamPreparation/Exam13Dec2019/02.DataImport/DataProcessor/ImportDto/ImportBookDto.cs
@@ -0,0 +1,43 @@
+﻿using BookShop.Data.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace BookShop.DataProcessor.ImportDto
+{
+    [XmlType("Book")]
+   public class ImportBookDto
+    {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(30)]
+        [XmlElement("Name")]
+        public string Name { get; set; }
+
+        [XmlElement("Genre")]
+        [Range(1,3)]
+        public int Genre { get; set; }
+
+        [Range(typeof(decimal),"0.01","79228162514264337593543950335")]
+        [XmlElement("Price")]
+        public decimal Price { get; set; }
+
+        [XmlElement("Pages")]
+        [Range(50,5000)]
+        public int Pages { get; set; }
+
+        [XmlElement("PublishedOn")]
+        public string PublishedOn { get; set; }
+    }
+}
+
+//< Books >
+//  < Book >
+//    < Name > Hairy Torchwood </ Name >
+//       < Genre > 3 </ Genre >
+//       < Price > 41.99 </ Price >
+//       < Pages > 3013 </ Pages >
+//       < PublishedOn > 01 / 13 / 2013 </ PublishedOn >
+//     </ Book >
